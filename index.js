2284650567514a5395df43c326d51ca8b6e412ce
zacharyccampbell <zacharydenzel23@gmail.com>
2023-09-06 15:03:58 -0500

    For help purposes

--- index.js
+++ index.js
@@ -6,23 +6,37 @@
 const container = document.querySelector(".container");
 const boxes = document.getElementsByClassName("child");
 const changeButton = document.querySelector("button");
-let numDivs = 256;
+let numSquares = 16;
+
+function numDivs(num) {
+    return num * num;
+};
 
 function makeDivs(numDivs) {
+    container.innerHTML = ''; // Clear the existing squares
+    const percentage = 100 / numSquares; // Calculate the percentage based on the number of squares
     for (i = 0; i < numDivs; i++) {
         let cells = document.createElement('div');
         container.appendChild(cells);
         cells.classList.add("child");
+        cells.style.width = `calc(${percentage}% - 2px)`; // Set the width using calculated percentage
+        cells.style.height = `calc(${percentage}% - 2px)`; // Set the height using calculated percentage
     }
 };
 
-makeDivs(numDivs);
+makeDivs(numDivs(numSquares));
 
 changeButton.addEventListener("click", () => {
-    let numSquares = prompt("How many squares per side? ");
-    if (numSquares <= 100) {
-        let numDivs = numSquares;
-        return numDivs;
+    let numUserSquares = prompt("How many squares per side? ");
+    if (numUserSquares <= 100 && !isNaN(numUserSquares)) {
+        let numSquares = numUserSquares;
+        makeDivs(numDivs(numSquares));
+        for (let i = 0; i < boxes.length; i++) {
+            boxes[i].addEventListener("mouseover", () => {
+                boxes[i].classList.add("green");
+                console.log("green");
+            })
+        };
     } else {
         alert("100 or below please!");
     }
